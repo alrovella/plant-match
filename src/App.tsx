d6f60712521be3e7ef2c6ce5d986633284315a7f
Ale Rovella <alrovella@gmail.com>
2024-11-22 11:41:39 -0300

    feat: app styling update

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import PlantForm from "./components/PlantForm";
 
 function App() {
   return (
-    <main className="flex flex-col justify-start items-center gap-4 md:gap-12 mx-auto pt-4 md:pt-24 max-w-xl h-screen">
-      <div className="flex flex-col items-center gap-0.5 md:gap-2">
+    <main className="flex flex-col justify-start items-center gap-4 md:gap-12 mx-auto px-4 md:px-0 pt-4 md:pt-24 max-w-xl h-screen">
+      <header className="flex flex-col items-center gap-0.5 md:gap-2">
         <Logo className="text-green-500 size-20" />
         <h1 className="font-bold text-2xl">
           PLANT <strong>MATCH</strong>
         </h1>
-      </div>
+      </header>
       <PlantForm />
     </main>
   );
